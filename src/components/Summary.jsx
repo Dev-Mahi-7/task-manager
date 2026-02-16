f314740c63d0b43277dc58a5e8e171a87942257f
Dev-Mahi-7 <developermahi9@gmail.com>
2026-02-16 23:10:19 +0530

    feat:"check the responsiveness of the task-manager app"

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -9,9 +9,9 @@ export default function Summary() {
 
   return (
     <div className="mt-4 flex items-center justify-around border-t border-t-gray-200 pt-4 text-sm text-gray-700">
-      <p className=" px-4 py-2 text-xl shadow-lg" >Total: {total}</p>
-      <p className=" px-4 py-2 text-xl shadow-lg" >Completed: {completed}</p>
-      <p className=" px-4 py-2 text-xl shadow-lg" >Remaining: {remaining}</p>
+      <p className=" px-4 py-2 text-lg md:text-xl shadow-lg" >Total: {total}</p>
+      <p className=" px-4 py-2 text-lg md:text-xl shadow-lg" >Completed: {completed}</p>
+      <p className=" px-4 py-2 text-lg md:text-xl shadow-lg" >Remaining: {remaining}</p>
     </div>
   );
 }
